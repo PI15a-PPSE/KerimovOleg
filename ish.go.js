48f42f2757389203190e55f5a4db0733202d65e9
KerimovOleg <oleg.s.kerimov@yandex.ua>
2018-11-19 20:26:41 +0300

    Finished main game code

--- ish.go.js
+++ ish.go.js
@@ -82,4 +82,75 @@ function Point(row, column) {
         return false;
     };
 }
+
+/**
+ * OBJ: Defines common attributes for a territory.
+ */
+function Territory(points, owner) {
+    this.points = points || new Array();
+    this.owner = owner || Constants.TerritoryOwner.UNKNOWN;
+}
+
+/**
+ * OBJ: Defines common attributes for a player.
+ */
+function Player(color, pointState, score) {
+    this.color = color;             // Constants.Color.(BLACK/WHITE)
+    this.pointState = pointState;   // Constants.PointState.(BLACK/WHITE)
+    this.score = score || 0;
+    
+    this.equals = function(other) {
+        return (this.color == other.color);
+    };  
+    this.toString = function() {
+        return this.color;
+    };
+}
+
+/**
+ * OBJ: Defines common attributes for a game of Go.
+ */
+function GameState(boardWidth, boardHeight, player1, player2, status) { 
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+    
+    // Initialize board
+    this.board = new Array(this.boardHeight);   
+    for (var i = 0; i < this.boardHeight; i++) {
+        this.board[i] = new Array(this.boardWidth);
+        for (var j = 0; j < this.boardWidth; j++) {
+            this.board[i][j] = Constants.PointState.EMPTY;
+        }
+    }
+    
+    this.previousBoard = $.extend(true, [], this.board);    
+    this.player1 = player1;
+    this.player2 = player2; 
+    this.currentPlayer = player1;   
+    this.moveError;
+    this.status = status || Constants.GameStatus.ACTIVE;
+    
+    this.getPointStateAt = function(point) {
+        return this.board[point.row][point.column];
+    };
+    this.setPointStateAt = function(point, pointState) {
+        this.board[point.row][point.column] = pointState;
+    };  
+    this.isUniqueBoard = function() {
+        // Compare board and previousBoard arrays
+        for (var y = 0; y < this.boardHeight; y++) {
+            for (var x = 0; x < this.boardWidth; x++) {
+                if (this.board[y][x] != this.previousBoard[y][x]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+    this.getBoardCopy = function() {
+        return $.extend(true, [], this.board)
+    };
+    this.setBoardCopy = function(board) {
+        this.board = $.extend(true, [], board);
+    };
 };
